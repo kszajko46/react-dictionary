06573ce9afcf61123e4a75ca0bbb44dbad230ff7
kszajko46 <khszajko46@gmail.com>
2022-04-08 10:59:05 -0400

    created dictionary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Daily Dictionary</header>
+      <div className="container">
+        <header className="App-header">Daily Dictionary</header>
+
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer className="text-center">
+        Coded by{" "}
+        <a href="https://www.linkedin.com/in/kata-szajko/">Kata Szajko </a>{" "}
+        published on{" "}
+        <a href="https://github.com/kszajko46/react-dictionary/find/main">
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
