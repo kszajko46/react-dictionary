96b16aaa984a61f09ac97addd81b9d88363fa506
kszajko46 <khszajko46@gmail.com>
2022-04-14 10:58:42 -0400

    creating function to find and display definitions of word searched

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,18 +5,16 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState({});
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
-    // documentation: https://dictionaryapi.dev/
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_us/${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -27,9 +25,9 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange}></input>
+        <input type="search" onChange={handleKeywordChange} />
       </form>
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Results.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Results.css";
+
+export default function Results(props) {
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meanings, index) {
+          return meanings.definitions[0].definition;
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
