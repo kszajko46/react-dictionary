26009277a4c0ec46a00cdd495ce5ec893ee008aa
kszajko46 <khszajko46@gmail.com>
2022-04-25 13:47:47 -0400

    styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,9 +24,14 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
+      <section>
+        <form onSubmit={search}>
+          <input type="search" onChange={handleKeywordChange} />
+        </form>
+        <div className="hint">
+          Suggested words: cake, sunset, candy, beach...
+        </div>
+      </section>
       <Results results={results} />
     </div>
   );
