246a963eb0ff0d71031bce55bfb725f3a4e5cf40
kszajko46 <khszajko46@gmail.com>
2022-04-25 13:27:38 -0400

    why is my target blank always a problem

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,9 +3,7 @@ import React from "react";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
-        listen
-      </a>
+      <a href={props.phonetic.audio}>listen</a>
       <br />
       {props.phonetic.text}
     </div>
